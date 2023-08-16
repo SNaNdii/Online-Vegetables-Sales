df67411ad63491ce9658c2d32f1f6892c5c033a4
SNaNdii <singhnandita73@gmail.com>
2023-08-17 00:41:30 +0530

    Update

---
+++ Online_Vegetables_sale/src/main/java/com/masai/exceptions/BillingDetailsException.java
@@ -0,0 +1,15 @@
+package com.masai.exceptions;
+
+public class BillingDetailsException extends Exception{
+	
+	public BillingDetailsException() {
+		
+		
+	}
+	
+	public BillingDetailsException(String msg) {
+		
+		super(msg);
+	}
+
+}
