8255bfdbedc49fbe3db163b8733087479dc31813
nandita singh <108002752+SNaNdii@users.noreply.github.com>
2023-08-17 01:55:02 +0530

    Delete Online_Vegetables_sale directory

--- Online_Vegetables_sale/src/main/java/com/masai/model/TransactionMode.java
+++
@@ -1,5 +0,0 @@
-package com.masai.model;
-
-public enum TransactionMode {
-	COD, PAYTM, PHONEPE,DEBITCARD,CREDITCARD, GOOGLEPAY
-}
--- Online_Vegetables_sale/src/main/java/com/masai/repository/CartDao.java
+++
@@ -1,9 +0,0 @@
-package com.masai.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.masai.model.Cart;
-
-public interface CartDao extends JpaRepository<Cart, Integer>{
-	
-}
