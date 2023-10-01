7d5d248e0b53764fda10b411c1e1caa2cb9b3e35
SNaNdii <singhnandita73@gmail.com>
2023-10-01 13:57:49 +0530

    Online vegetables sales management system project completed

--- OnlineVegetablesSales/src/main/java/com/masai/controller/BillingDetailsController.java
+++ OnlineVegetablesSales/src/main/java/com/masai/controller/BillingDetailsController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.masai.exceptions.BillingDetailsException;
 import com.masai.model.BillingDetails;
 import com.masai.service.BillingsService;
